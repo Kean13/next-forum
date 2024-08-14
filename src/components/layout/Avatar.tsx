8bcdab720a5afd739c9b51d4a40fcad2600cc238
Kean <kean@octo.gg>
2024-08-15 04:41:14 +0500

    🐛 Фикс ошибки

--- src/components/layout/Avatar.tsx
+++ src/components/layout/Avatar.tsx
@@ -10,7 +10,7 @@ import {
 	DropdownMenuLabel,
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+} from '@/components/ui/DropdownMenu'
 
 interface MenuItem {
 	label: string
